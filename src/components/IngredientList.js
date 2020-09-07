098039f5211c02da784c19765e9079f78a4bc9bc
justinsong99 <jsong69@jhu.edu>
2020-09-07 14:39:21 -0400

    Worked on search bar for the ingredient list, now shows up and used state feature to make page more dynamic

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IngredientCard from './IngredientCard';
+import IngredientSearchBar from './IngredientSearchBar';
 
 import './css/IngredientList.css';
 
@@ -8,15 +9,36 @@ import pineapple from '../res/pineapple.png';
 import tomato from '../res/tomato.png';
 
 class IngredientList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { ingredients: [{"name":cheese}, {"name":pineapple}, {"name":tomato}] }	
+	}
+
+	onSearchSubmit = async (term) => {
+		if (term === "cheese") {
+			term = cheese;
+		} else if (term === "tomato") {
+			term = tomato;
+		} else if (term === "pineapple") {
+			term = pineapple;
+		}
+		
+		//Empty string so empty field when onSubmit was triggered
+		if(term === "") {
+			//We set state to the default list of ingredients
+			this.setState({ ingredients: [{"name":cheese}, {"name":pineapple}, {"name":tomato}] });
+		} else {
+			this.setState({ ingredients: [{"name": term}] });
+		}
+	}
+
 	render() {
-		const arrayOfIngredients = [{"name":cheese}, {"name":pineapple}, {"name":tomato}];
 		return (
 			<div className="container2">
-				<form className="ui form">
-					<input type="text" />
-				</form>	
+				<IngredientSearchBar onSubmit={this.onSearchSubmit}/>
 				<div className="list-styling">
-					{arrayOfIngredients.map((d, idx) =>
+					{this.state.ingredients.map((d, idx) =>
 						<IngredientCard
 							ingredient={d.name}
 							key={idx}
